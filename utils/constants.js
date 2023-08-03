602e50b0e4c848e19f96dc9ad48fdac369804af4
ibragim-ast <ibragim.ast@gmail.com>
2023-08-03 11:39:09 +0300

    add movieSchema

--- utils/constants.js
+++ utils/constants.js
@@ -1,8 +1,10 @@
+const URL_REGEX = /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]+\.[a-zA-Z0-9()]+\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)/;
 const ERROR_401 = 401;
 
 const INVALID_AUTH_DATA_ERROR_MESSAGE = 'Неправильные почта или пароль';
 
 module.exports = {
+  URL_REGEX,
   ERROR_401,
   INVALID_AUTH_DATA_ERROR_MESSAGE,
 };
